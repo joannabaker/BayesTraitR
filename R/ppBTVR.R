a97c9a73f0486145dd90cf30f41644b767f5d708
joannabaker <joanna.lab@gmail.com>
2024-03-20 15:05:21 +0000

    add summarizeVR functionality

--- R/ppBTVR.R
+++ R/ppBTVR.R
@@ -152,14 +152,107 @@ readVR <- function(vrfile){
 
   }
 
-
   # Tidy VR block
   vrlogtable = tidyVRblock(.vrlog, .colstart)
 
   # Add descendant information to table
   brInfotable$Des = unlist(sapply(brDes,function(x)paste0(sort(x), collapse = ",")))
 
+  # Now also add all branches descending to the table, too
+  alldes = lapply(brInfotable$Des, function(x)unlist(strsplit(x,split=",")))
+  brInfotable$DesBranches = unlist(lapply(alldes,function(y)paste0(brInfotable$branch[which(unlist(lapply(alldes,function(x)all(x %in% y))))], collapse = ",")))
+
   return(list(brInfo = brInfotable, VRlog = vrlogtable, ntrees = ntrees))
 }
 
 
+
+#' @title Process BayesTraits VR log file
+#' @description Process the output files from a variable rates analysis in BayesTraits
+#'     as read in by readVR.
+#' @param VRout An R object defining the output of the read VR function.
+#' @export
+processVR = function(VRout){
+
+  # If multiple trees, change the column names for consistency
+  if(VRout$ntrees !=1) {VRout$VRlog$NodeID = VRout$VRlog$PartID; VRout$VRlog$PartID = NULL}
+
+
+  # Identify the unique iterations
+  iters = unique(VRout$VRlog$It)
+
+  # Create a summary table
+  ratestable_summary = data.frame(VRout$brInfo)
+  ratestable_summary[,c("median_scalar", "mean_scalar")] = NA
+  ratestable_summary[,c("n_scaled", "n_origin" )] = 0
+
+  # Create a table for the posteriors
+  ratestable_post = as.data.frame(matrix(1, ncol=nrow(ratestable_summary), nrow=length(unique(iters)), dimnames=list(NULL, ratestable_summary$branch)))
+  rownames(ratestable_post) = iters
+
+
+
+  # Now traverse the VRlog file and apply scalars to each branch at each iteration
+  for(i in 1:nrow(VRout$VRlog)){
+
+    # Identify iteration
+    iter = VRout$VRlog$It[i]
+
+    # If NO rate scalars identified, move on
+    if(is.na(VRout$VRlog$Scaler[i])) next
+
+    # Identify branch
+    branch = origin = ratestable_summary$branch[which(ratestable_summary$branch == VRout$VRlog$NodeID[i])]
+
+    # If this is a node scalar, this needs to be applied across all branches
+    if(VRout$VRlog$NodeBranch[i] == "Node"){
+      branch = unlist(strsplit(ratestable_summary$DesBranches[ratestable_summary$branch == branch], ","))
+    }
+
+    # Apply the rate scalar
+    ratestable_post[iter,branch] = ratestable_post[iter,branch]* as.numeric(VRout$VRlog$Scaler[i])
+
+    # Add n to the summary table
+    ratestable_summary[branch, "n_scaled"] = ratestable_summary[branch, "n_scaled"] + 1
+    ratestable_summary[origin, "n_origin"] = ratestable_summary[origin, "n_origin"] + 1
+
+  }
+
+  # Calculate the mean and median rate scalars
+  ratestable_summary$median_scalar = apply(ratestable_post,2, median)
+  ratestable_summary$mean_scalar = apply(ratestable_post,2, mean)
+
+  return(list(summary = ratestable_summary, posterior = ratestable_post))
+
+}
+
+#' @title Summarize BayesTraits VR Log Files (from raw output)
+#' @description Reads the output file from a variable rates analysis in BayesTraits
+#'     and produces summarized output. Will only work with VR log files
+#'     (.VarRates.txt) as directly output from BayesTraits models 1-4, 7 and 9.
+#'     Will not (currently) work with output from any other program or model.
+#' @param vrfile Takes a single string defining the direct path to a single log file.
+#' @returns A list with two elements:
+#' \itemize{
+#' \item summary - A \code{data.frame} giving information about each branch/node
+#' in the analysis. Each branch is identified by the numeric value in "branch". For
+#' samples of trees, this value is equivalent to the "partition ID" used internally
+#' by BayesTraits. "NoDes" and "Des" record the number and names of all descendant
+#' taxa from each branch. The "freq" and "probability" columns tell us the frequency
+#' and probability that this branch occur in the sample are only relevant for
+#' analyses over samples of trees. The "length" records the branch length and
+#' is left blank for tree samples. The "DesBranches" tells us all branches that descend from
+#' the branch of interest. The median and mean scalar are the median and mean rate
+#' scalars acting on that branch across the entire posterior distribution. n_scaled
+#' tells us how many iterations that branch had a scalar acting on it, and n_origin tells
+#' us how many iterations that branch had a scalar ORIGINATING on it (either as the base of
+#' a node scalar or as its own branch scalar).
+#' \item posterior - A \code{data.frame} providing the posterior distribution of rate scalars acting
+#' on every branch (column) at each iteration (row).
+#' }
+#' @export
+summarizeVR <- function(vrfile){
+  VRout = readVR("VRlogfile-multitop-ET.txt")
+  VRsum = processVR(VRout)
+  return(VRsum)
+}
