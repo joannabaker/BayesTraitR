7448c664121052a7114498544580531bd90fd374
joannabaker <joanna.lab@gmail.com>
2025-07-24 16:49:28 +0100

    adjust csv output

--- R/geoTools.R
+++ R/geoTools.R
@@ -49,7 +49,7 @@ sf_to_csv <- function(age = 150, grid = NULL, extent = c(-180, 180, -90, 90),
   df <- as.data.frame(map_rast, xy = TRUE, na.rm = FALSE)
   colnames(df) <- c("lon", "lat", "mask")
 
-  if(outputfile != "") write.csv(df, file = df, header = T)
+  if(outputfile != "") write.csv(df, file = df, col.names = T, row.names = F, quote = F)
 
   # Return data frame
   return(df)
